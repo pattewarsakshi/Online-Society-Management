f105fee8a16778498aa2e4f4b793b0bf088be3ba
sukirti sharma <sukirtisharma74@gmail.com>
2026-02-05 06:28:37 +0530

    refracted main class name

---
+++ Backend/society-management/src/main/java/com/society/management/SocietyManagementApplication.java
@@ -0,0 +1,18 @@
+package com.society.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication(
+    exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class
+    }
+)
+@EnableScheduling
+public class SocietyManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocietyManagementApplication.class, args);
+    }
+}
